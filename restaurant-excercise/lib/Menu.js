f1d715f100d65fc2628e9ddab8c779dae1c7ecdb
eripheebs <erikapheby@gmail.com>
2016-08-15 16:36:51 +0100

    items can be deleted as an array, using recursion

--- restaurant-excercise/lib/Menu.js
+++ restaurant-excercise/lib/Menu.js
@@ -1,6 +1,30 @@
+require('./polyfills/includes.js')
+
 var Menu = {
   init: function(items){
     this.items = [].concat.apply([], items)
+  },
+  addItems: function(items){
+    this.items = [].concat.apply(this.items, items)
+  },
+  deleteItem: function(items){
+    var self = this;
+    var checkIfIncluded = function(item){
+      return items.includes(item);
+    }
+
+    var thereIsStillItemIncluded = function(){
+      return !(count == self.items.length)
+    }
+
+    var count = 0;
+    self.items.forEach(function(item, index){
+      checkIfIncluded(item) ? self.items.splice(index, 1) : count++
+    });
+
+    if (thereIsStillItemIncluded()) {
+      self.deleteItem(items)
+    };
   }
 };
 
--- restaurant-excercise/spec/MenuSpec.js
+++ restaurant-excercise/spec/MenuSpec.js
@@ -1,13 +1,30 @@
 describe("Menu", function(){
   var Menu = require('../lib/Menu.js');
-  var menu;
 
   beforeEach(function(){
     menu = Object.create(Menu);
     menu.init();
+    item = jasmine.createSpy('Item');
+    items = [item, item];
   });
 
   it("can be initialised without items", function(){
     expect(menu.items).toEqual([]);
   })
+
+  it("can add items to the menu", function(){
+    menu.addItems(items);
+    expect(menu.items).toEqual(items);
+  });
+
+  it("can delete items off the menu (so 3 of them will be deleted in this case)", function(){
+    menu.addItems(items);
+    item2 = jasmine.createSpy('Item 2');
+    item3 = jasmine.createSpy('Item 3');
+    items2 = [item2, item3];
+    menu.addItems(items2);
+    menu.deleteItem([item, item3]);
+    expect(menu.items).not.toContain(item);
+    expect(menu.items).not.toContain(item3);
+  });
 });
